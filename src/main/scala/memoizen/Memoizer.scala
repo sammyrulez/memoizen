5a82dc45b94277a786d95942e452164c4228d07e
sammyrulez <sammyrulez@gmail.com>
2015-02-02 15:10:19 +0100

    factory support

--- src/main/scala/memoizen/Memoizer.scala
+++ src/main/scala/memoizen/Memoizer.scala
@@ -1,10 +1,24 @@
 package memoizen
 
+
+case class BasicMemoizerConf(val size:Option[Int] = None)
+case class RedisMemoizerConf(val host: String,val port:Int = 6379)
+
+
 object Memoizer {  
 
-  def apply[A, B](f: A => B):Memoizer[A, B] = {
-     new MapMemoizer(f)
+
+  
+  def apply[A, B : Manifest](f: A => B, conf:Any):Memoizer[A, B] = {
+    
+    conf match {
+      case b:BasicMemoizerConf => new MapMemoizer(f,b.size)
+      case r:RedisMemoizerConf => new RedisBackedMemoizer(f, r.host,r.port)
+    }
+    
   }
+  
+  
   
 }
 
---
+++ src/test/scala/memoizen/FactorySpec.scala
@@ -0,0 +1,27 @@
+package memoizen
+
+import org.specs2.mutable.Specification
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class FactorySpec extends Specification{
+  
+   def strSqLen(s: String) = {s.length*s.length }
+
+  "The 'Memoizer'" should {
+      "create MapMemoizer upon request" in {      
+        val strSqLenMemoized =  Memoizer(strSqLen,new BasicMemoizerConf)
+        
+        strSqLenMemoized.getClass.getName must equalTo("memoizen.MapMemoizer")
+      }
+       "create RedisMemoizer upon request" in {      
+        val strSqLenMemoized =  Memoizer(strSqLen,new RedisMemoizerConf("localhost"))
+        
+        strSqLenMemoized.getClass.getName must equalTo("memoizen.RedisBackedMemoizer")
+      }
+  }
+  
+  
+}
